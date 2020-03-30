5af68f2cdd08d9e33ff26c36e9f01d55bf1d1924
Kindface <kindface97@gmail.com>
2020-03-30 19:25:35 +0300

    added LoginRequiredMixin for workouts

--- workouts/views.py
+++ workouts/views.py
@@ -1,52 +1,29 @@
-from django.views.generic import ListView, DeleteView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Workout
-from exercises.models import Exercise
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, get_object_or_404
 from .forms import WorkoutCreateForm
 from django.shortcuts import render
 from django.urls import reverse
+from django.contrib.auth.mixins import *
 
-class WorkoutListView(ListView):
+
+class WorkoutListView(LoginRequiredMixin, ListView):
     model = Workout
     template_name = 'workouts.html'
     paginate_by = 5
+    login_url = '/login'
 
     def get_queryset(self):
         user = self.request.user
         queryset = Workout.objects.filter(owner=user).order_by("-id")
         return queryset
-'''
-def workoutCreate(request):
-    form = WorkoutCreateForm(request.user)
-    if form.is_valid():
-        date = request.POST.get("date")
-        print(date)
-        form.save()
-        return render(request, 'workouts.html', {'form': form})
-    else:
-        return render(request, 'workout_add.html', {'form': form})
 
 
-
-
-class WorkoutCreateView(CreateView):
-    model = Workout
-    fields = ('date', 'description', 'exercises')
-    template_name = 'workout_add.html'
+class WorkoutCreateView(LoginRequiredMixin, CreateView):
     success_url = 'workouts'
-
-    def form_valid(self, form):
-        self.object = form.save(commit=False)
-        self.object.owner = self.request.user
-        self.object.save()
-        return HttpResponseRedirect(self.get_success_url())
-'''
-
-
-class WorkoutCreateView(CreateView):
-    success_url = 'workouts'
+    login_url = '/login'
 
     def get(self, request, *args, **kwargs):
         context = {'form': WorkoutCreateForm(user=request.user)}
@@ -62,43 +39,31 @@ class WorkoutCreateView(CreateView):
             return HttpResponseRedirect('workouts')
         return render(request, 'workout_add.html', {'form': form})
 
-'''
-class WorkoutUpdateView(UpdateView):
-    def get(self, request, *args, **kwargs):
-        context = {'form': WorkoutCreateForm(user=request.user)}
-        return render(request, 'edit_workout.html', context)
-
-    def put(self, request, *args, **kwargs):
-        form = WorkoutCreateForm(request.POST, user=request.user)
-        if form.is_valid():
-            workout = form.save(commit=False)
-            workout.owner = request.user
-            workout.save()
-            form.save_m2m()
-            return HttpResponseRedirect('workouts')
-        return render(request, 'edit_workout.html', {'form': form})
-'''
 
 def workout_edit(request, pk):
-    #global form
-    workout = get_object_or_404(Workout, pk=pk)
-    if request.method == "POST":
-        form = WorkoutCreateForm(request.POST, user=request.user, instance=workout)
-        if form.is_valid():
-            workout = form.save(commit=False)
-            workout.owner = request.user
-            workout.save()
-            form.save_m2m()
-            return redirect('workout_detail', pk=workout.pk)
+    if not request.user.is_authenticated:
+        return redirect('/login')
     else:
-        form = WorkoutCreateForm(user=request.user,instance=workout)
-    return render(request, 'edit_workout.html', {'form': form})
+        workout = get_object_or_404(Workout, pk=pk)
+        if request.method == "POST":
+            form = WorkoutCreateForm(request.POST, user=request.user, instance=workout)
+            if form.is_valid():
+                workout = form.save(commit=False)
+                workout.owner = request.user
+                workout.save()
+                form.save_m2m()
+                return redirect('workout_detail', pk=workout.pk)
+        else:
+            form = WorkoutCreateForm(user=request.user, instance=workout)
+        return render(request, 'edit_workout.html', {'form': form})
 
-class WorkoutDetailView(DeleteView):
+
+class WorkoutDetailView(LoginRequiredMixin, DetailView):
     model = Workout
     template_name = 'workout_detail.html'
+    login_url = '/login'
 
 
 def delete_workout(self, pk):
-    workout = Workout.objects.get(pk=pk).delete()
+    Workout.objects.get(pk=pk).delete()
     return HttpResponseRedirect(reverse('workouts'))
