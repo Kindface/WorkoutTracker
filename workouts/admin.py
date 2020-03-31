788d52e2cead08a1de4ae45a8c27b151586ee87d
Kindface <kindface97@gmail.com>
2020-03-31 20:35:05 +0300

    remove workouts from admin site

--- workouts/admin.py
+++ workouts/admin.py
@@ -1,14 +1,1 @@
 from django.contrib import admin
-from .models import Workout, Exercise
-
-
-class WorkoutAdmin(admin.ModelAdmin):
-    list_display = ['date', 'owner', 'description']
-
-
-class ExerciseAdmin(admin.ModelAdmin):
-    list_display = ['name', 'sets', 'reps']
-
-
-admin.site.register(Workout, WorkoutAdmin)
-admin.site.register(Exercise, ExerciseAdmin)
